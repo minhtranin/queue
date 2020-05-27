bd029034ee97e8251b0c8c7d4dc0c5dc4793a554
MinhTran <minhtran.in@outlook.com>
2020-05-27 19:08:46 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var q = 'tasks';
+
+var open = require('amqplib').connect('amqp://localhost');
+open.then(function(conn) {
+    return conn.createChannel();
+  }).then(function(ch) {
+    return ch.assertQueue(q).then(function(ok) {
+      return ch.sendToQueue(q, Buffer.from('something to do'));
+    });
+  }).catch(console.warn);
+
+  // Consumer
+  open.then(function(conn) {
+    return conn.createChannel();
+  }).then(function(ch) {
+    return ch.assertQueue(q).then(function(ok) {
+      return ch.consume(q, function(msg) {
+        if (msg !== null) {
+          console.log(msg.content.toString());
+          ch.ack(msg);
+        }
+      });
+    });
+  }).catch(console.warn);
